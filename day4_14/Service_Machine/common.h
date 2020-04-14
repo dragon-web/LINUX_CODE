65d54826dfed4ea5111655dbec1d04881cd21fd4
dragon-web <root@localhost.localdomain>
2020-04-14 11:34:50 -0400

    day4_14/

---
+++ day4_14/Service_Machine/common.h
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<assert.h>
+#include<stdlib.h>
+#include<assert.h>//断言
+#include<stdbool.h>
+#include<string.h>
+
---
+++ day4_14/Service_Machine/machine.c
@@ -0,0 +1,12 @@
+#include"common.h"
+
+
+int main()
+{
+   int i = 1;
+   printf("%d,%d\n",++i,++i);
+   printf("%d,%d",++i,i++);
+  return 0;
+}
+
+
