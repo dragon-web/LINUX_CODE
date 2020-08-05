9e3d0d2129c73dbf0504052d88c5d6fb16c3f940
dragon-web <1582943265@qq.com>
2020-08-05 16:52:55 +0800

    Function_Point/

--- Function_Point/test.c
+++ Function_Point/test.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 //指向函数的指针变量没有指针的++ --
 //函数指针中的形参可有可无
-
+/*
 int Find_Max(int a,int b)
 {
   return a > b ? a : b;
@@ -17,6 +19,37 @@ int main(int argc,char* argv[])
  printf("num1 = %d,num2 = %d,num3 = %d\n",num1,num2,num3); 
   return 0;
 }
+*/
+
+int get_rand_num()
+{
+  return rand();
+}
+void test(int *array,size_t array_size,int(*p)())
+{
+  size_t i;
+  for( i = 0;i < array_size;i++)
+  {
+    array[i] = get_rand_num();
+  }
+}
+
+int main()
+{
+  int i;
+  int array[10];
+  test(array,10,get_rand_num);
+  for(i = 0;i < 10;++i)
+  {
+    printf("%d  ",array[i]);
+  }
+  return 0;
+}
+
+
+
+
+
 
 
 
